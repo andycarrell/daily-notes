1536a56891a3415cd1e39e323d520a154c1105c8
andycarrell <carrellandrew@gmail.com>
2021-10-09 14:29:58 +1300

    Extracts page & layout components, creates 'card'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
+
+const Page = ({ children }) => (
+  <div className="bg-gray-800 min-h-screen">{children}</div>
+);
+
+const Layout = ({ children }) => (
+  <div className="h-full flex flex-col items-center px-4 sm:px-0 pt-[25vh]">
+    {children}
+  </div>
+);
 
 const App = () => {
   const [date] = useState(() => new Date());
 
   return (
-    <div className="py-5 px-3">
-      <h1 className="text-3xl font-serif font-bold mb-6">
-        {date.toDateString()}
-      </h1>
-    </div>
+    <Page>
+      <Layout>
+        <div className="max-w-xl w-full border-2 border-gray-600 bg-gray-700 bg-opacity-40 shadow-md rounded-lg p-5">
+          <h1 className="text-3xl font-serif text-gray-300 font-bold mb-6">
+            {date.toDateString()}
+          </h1>
+        </div>
+      </Layout>
+    </Page>
   );
 };
 
