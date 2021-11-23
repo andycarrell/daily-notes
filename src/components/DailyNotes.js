ca1ae826ca97a96e5fa032924b9911c8f8bf3630
andycarrell <carrellandrew@gmail.com>
2021-11-24 10:15:12 +1300

    Adds initial implementation of date (d) query param

--- src/components/DailyNotes.js
+++ src/components/DailyNotes.js
@@ -8,6 +8,27 @@ import Note from "./Note";
 import { Layout, Card } from "./layout";
 import { IconGrayButton } from "./Button";
 
+const PARAM = "d";
+const START_OF_DAY_ISO = "T00:00:00.000Z";
+
+const getSearchParam = () => {
+  const url = new URL(window.location);
+  const value = url.searchParams.get(PARAM);
+  return value;
+};
+
+const updateSearchParam = (value) => {
+  const url = new URL(window.location);
+  url.searchParams.set(PARAM, value);
+  window.history.pushState("", "", url.href);
+};
+
+const deleteSearchParam = () => {
+  const url = new URL(window.location);
+  url.searchParams.delete(PARAM);
+  window.history.pushState("", "", url.href);
+};
+
 const startOfUTCTodayToISOString = () => {
   const today = new Date();
   const year = today.getFullYear();
@@ -17,7 +38,7 @@ const startOfUTCTodayToISOString = () => {
   const paddedDay =
     ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"][day] || day;
 
-  return `${year}-${month}-${paddedDay}T00:00:00.000Z`;
+  return `${year}-${month}-${paddedDay}${START_OF_DAY_ISO}`;
 };
 
 const displayFromISOString = (date) => {
@@ -26,12 +47,28 @@ const displayFromISOString = (date) => {
   return new Date(stripZOffset).toDateString();
 };
 
+const getInitialDate = () => {
+  const raw = getSearchParam();
+  if (raw) {
+    return `${raw}${START_OF_DAY_ISO}`;
+  }
+  return startOfUTCTodayToISOString();
+};
+
 const DailyNotes = () => {
-  const [date, dispatch] = useDateReducer(startOfUTCTodayToISOString);
+  const [date, dispatch] = useDateReducer(getInitialDate);
 
   useEffect(() => {
     document.title = `${displayFromISOString(date)} - daily-notes`;
   }, [date]);
+
+  useEffect(() => {
+    const [rawDate] = date.split(START_OF_DAY_ISO);
+    updateSearchParam(rawDate);
+  }, [date]);
+
+  // Remove search param on unmount
+  useEffect(() => deleteSearchParam, []);
 
   return (
     <Layout className="max-w-2xl">
