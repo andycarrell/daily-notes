696b5da3fe8bf10a0b279ca9925b3f63e88f076c
andycarrell <carrellandrew@gmail.com>
2022-02-07 17:36:21 +1300

    Adds suspense for data fetching - no fallbacks no worries

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ClipboardListIcon, DocumentTextIcon } from "@heroicons/react/solid";
 
@@ -7,56 +7,64 @@ import { Page } from "./components/layout";
 import DailyNotes from "./components/DailyNotes";
 import { IconGrayButton } from "./components/Button";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const App = () => {
-  const [variant, setVariant] = useState("notes");
+  const [variant, setVariant] = useState("feed");
 
   return (
     <div className="antialiased">
       <QueryClientProvider client={queryClient}>
         <Page>
-          <div
-            role="tablist"
-            className="bg-gray-800 lg:bg-transparent w-full sm:sticky inset-0 z-10 flex flex-row justify-end p-4"
-          >
+          <Suspense fallback={null}>
+            <div
+              role="tablist"
+              className="bg-gray-800 lg:bg-transparent w-full sm:sticky inset-0 z-10 flex flex-row justify-end p-4"
+            >
+              {variant === "feed" ? (
+                <IconGrayButton
+                  role="tab"
+                  aria-selected="true"
+                  aria-controls="daily-notes"
+                  aria-label="Show daily notes"
+                  onClick={() => {
+                    setVariant("notes");
+                  }}
+                >
+                  <ClipboardListIcon className="h-8 w-8" />
+                </IconGrayButton>
+              ) : null}
+              {variant === "notes" ? (
+                <IconGrayButton
+                  role="tab"
+                  aria-selected="true"
+                  aria-controls="feed"
+                  aria-label="Show feed"
+                  onClick={() => {
+                    setVariant("feed");
+                  }}
+                >
+                  <DocumentTextIcon className="h-8 w-8" />
+                </IconGrayButton>
+              ) : null}
+            </div>
             {variant === "feed" ? (
-              <IconGrayButton
-                role="tab"
-                aria-selected="true"
-                aria-controls="daily-notes"
-                aria-label="Show daily notes"
-                onClick={() => {
-                  setVariant("notes");
-                }}
-              >
-                <ClipboardListIcon className="h-8 w-8" />
-              </IconGrayButton>
+              <div role="tabpanel" id="feed">
+                <Feed />
+              </div>
             ) : null}
             {variant === "notes" ? (
-              <IconGrayButton
-                role="tab"
-                aria-selected="true"
-                aria-controls="feed"
-                aria-label="Show feed"
-                onClick={() => {
-                  setVariant("feed");
-                }}
-              >
-                <DocumentTextIcon className="h-8 w-8" />
-              </IconGrayButton>
+              <div role="tabpanel" id="daily-notes">
+                <DailyNotes />
+              </div>
             ) : null}
-          </div>
-          {variant === "feed" ? (
-            <div role="tabpanel" id="feed">
-              <Feed />
-            </div>
-          ) : null}
-          {variant === "notes" ? (
-            <div role="tabpanel" id="daily-notes">
-              <DailyNotes />
-            </div>
-          ) : null}
+          </Suspense>
         </Page>
       </QueryClientProvider>
     </div>
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,10 @@
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import { customAlphabet } from "nanoid";
 
 import { ViewGridAddIcon } from "@heroicons/react/solid";
 
 import useFeedQuery from "../api/useFeedQuery";
 import useFeedMutation from "../api/useFeedMutation";
-
-import useSearchParam from "../utilities/useSearchParam";
 
 import Note from "./Note";
 import { Layout } from "./layout";
@@ -15,34 +13,19 @@ import DeferUntilViewport from "./DeferUntilViewport";
 
 const nanoid = customAlphabet("1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ", 6);
 
-const ScrollElementIntoView = ({ id }) => {
-  useEffect(() => {
-    const node = document.getElementById(id);
-
-    if (node) {
-      const id = setTimeout(
-        () =>
-          node.scrollIntoView({
-            block: "start",
-            inline: "nearest",
-            behavior: "smooth",
-          }),
-        300
-      );
-
-      return () => clearTimeout(id);
-    }
-  }, [id]);
-
-  return null;
-};
+const FeedItem = ({ id }) => (
+  <div className="flex flex-col-reverse pt-6">
+    <h2 className="self-end text-sm text-gray-500 font-semibold my-2 mr-1">
+      {id}
+    </h2>
+    <Note id={`feed-${id}`} />
+  </div>
+);
 
 const Feed = () => {
-  const { isError, isLoading, data } = useFeedQuery();
+  const { isError, data } = useFeedQuery();
   const { mutate } = useFeedMutation();
   const ids = data?.item ?? [];
-
-  const [getSearchParam] = useSearchParam("f");
 
   useEffect(() => {
     document.title = "Feed - daily-notes";
@@ -65,25 +48,24 @@ const Feed = () => {
           <ViewGridAddIcon className="h-7 w-7 sm:h-8 sm:w-8" />
         </IconGrayButton>
       </div>
-      {isLoading ? null : (
-        <div className="w-full divide-y divide-gray-600">
-          {ids.map((id) => (
-            <div id={id} key={id} className="flex flex-col-reverse pt-6">
-              <h2 className="self-end text-sm text-gray-500 font-semibold my-2 mr-1">
-                {id}
-              </h2>
-              <div className="min-h-[16rem]">
-                <DeferUntilViewport>
-                  <Note id={`feed-${id}`} />
-                </DeferUntilViewport>
-              </div>
-            </div>
-          ))}
-          {/** Temporarily here until content can be migrated */}
-          <Note id="feed" className="py-6" />
-          <ScrollElementIntoView id={getSearchParam()} />
-        </div>
-      )}
+      <div className="w-full divide-y divide-gray-600">
+        {ids.map((id, index) => (
+          <div key={id} className="min-h-[16rem]">
+            {/** Render the first 3 items - assume these are 'above the fold' */}
+            {index < 3 ? (
+              <FeedItem id={id} />
+            ) : (
+              <DeferUntilViewport>
+                <Suspense fallback={null}>
+                  <FeedItem id={id} />
+                </Suspense>
+              </DeferUntilViewport>
+            )}
+          </div>
+        ))}
+        {/** Temporarily here until content can be migrated */}
+        <Note id="feed" className="py-6" />
+      </div>
     </Layout>
   );
 };
