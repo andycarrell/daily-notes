6dcc25db2956bde1985c418b77b3098f52803f1e
andycarrell <carrellandrew@gmail.com>
2022-06-19 12:53:43 +1200

    Add Link w. styles to button file

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,28 @@
 import React from "react";
 
+const buttonClassName = (c) =>
+  `cursor-pointer flex justify-center items-center rounded-md transition-colors duration-200 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70 ${c}`.trim();
+const iconGrayClassName = (c) =>
+  "text-gray-400 hover:text-gray-300 focus-visible:ring-1 focus-visible:ring-gray-300 [&_svg]:h-8 [&_svg]:w-8";
+
 const Button = ({ className, children, ...rest }) => (
-  <button
-    className={`cursor-pointer flex justify-center items-center rounded-md transition-colors duration-200 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70 ${className}`}
-    {...rest}
-  >
+  <button className={buttonClassName(className)} {...rest}>
     {children}
   </button>
 );
 
+export const Link = ({ className, children, ...rest }) => (
+  <a className={buttonClassName(className)} {...rest}>
+    {children}
+  </a>
+);
+
+export const IconGrayLink = (props) => (
+  <Link className={iconGrayClassName()} {...props} />
+);
+
 export const IconGrayButton = (props) => (
-  <Button
-    className="text-gray-400 hover:text-gray-300 focus-visible:ring-1 focus-visible:ring-gray-300"
-    {...props}
-  />
+  <Button className={iconGrayClassName()} {...props} />
 );
 
 export default Button;
