eb5aa84797a3935354cab530780c413582865954
andycarrell <carrellandrew@gmail.com>
2022-06-19 13:18:18 +1200

    Adds download notes 'button' which converts the notes to json and proivdes them in a link format

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Feed from "./components/Feed";
 import { Page } from "./components/layout";
 import DailyNotes from "./components/DailyNotes";
 import { IconGrayButton } from "./components/Button";
+import DownloadNotes from "./components/DownloadNotes";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,36 +24,36 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <Page>
           <Suspense fallback={null}>
-            <div
-              role="tablist"
-              className="bg-gray-800 lg:bg-transparent w-full sm:sticky inset-0 z-10 flex flex-row justify-end p-4"
-            >
-              {variant === "feed" ? (
-                <IconGrayButton
-                  role="tab"
-                  aria-selected="true"
-                  aria-controls="daily-notes"
-                  aria-label="Show daily notes"
-                  onClick={() => {
-                    setVariant("notes");
-                  }}
-                >
-                  <ClipboardListIcon className="h-8 w-8" />
-                </IconGrayButton>
-              ) : null}
-              {variant === "notes" ? (
-                <IconGrayButton
-                  role="tab"
-                  aria-selected="true"
-                  aria-controls="feed"
-                  aria-label="Show feed"
-                  onClick={() => {
-                    setVariant("feed");
-                  }}
-                >
-                  <DocumentTextIcon className="h-8 w-8" />
-                </IconGrayButton>
-              ) : null}
+            <div className="bg-gray-800 lg:bg-transparent w-full sm:sticky inset-0 z-10 flex flex-row justify-end p-4 space-x-2">
+              {variant === "feed" ? <DownloadNotes /> : null}
+              <div role="tablist">
+                {variant === "feed" ? (
+                  <IconGrayButton
+                    role="tab"
+                    aria-selected="true"
+                    aria-controls="daily-notes"
+                    aria-label="Show daily notes"
+                    onClick={() => {
+                      setVariant("notes");
+                    }}
+                  >
+                    <ClipboardListIcon />
+                  </IconGrayButton>
+                ) : null}
+                {variant === "notes" ? (
+                  <IconGrayButton
+                    role="tab"
+                    aria-selected="true"
+                    aria-controls="feed"
+                    aria-label="Show feed"
+                    onClick={() => {
+                      setVariant("feed");
+                    }}
+                  >
+                    <DocumentTextIcon />
+                  </IconGrayButton>
+                ) : null}
+              </div>
             </div>
             {variant === "feed" ? (
               <div role="tabpanel" id="feed">
---
+++ src/components/DownloadNotes.js
@@ -0,0 +1,73 @@
+import React, { useMemo, useState } from "react";
+import {
+  ClipboardCopyIcon,
+  DocumentDownloadIcon,
+  DocumentIcon,
+  RefreshIcon,
+} from "@heroicons/react/solid";
+
+import useFeedQuery from "../api/useFeedQuery";
+import { useNotesQuery } from "../api/useNoteQuery";
+
+import { IconGrayButton, IconGrayLink } from "./Button";
+
+// todo: make ids / keys a prop
+const feedKeyFrom = (key) => `feed-${key}`;
+
+const DownloadNotes = () => {
+  // todo: add state for refresh - reset to idle every 5 minutes?
+  const [state, setState] = useState("idle");
+  const { data: feed } = useFeedQuery();
+  const { data, isFetching } = useNotesQuery(
+    (feed?.item ?? []).map(feedKeyFrom),
+    {
+      enabled: state === "load",
+      suspense: false,
+    }
+  );
+
+  const jsonString = useMemo(() => {
+    const encoded = encodeURIComponent(JSON.stringify(data));
+    return `data:text/json;charset=utf-8,${encoded}`;
+  }, [data]);
+
+  if (data) {
+    return (
+      <IconGrayLink
+        href={jsonString}
+        aria-label="Download notes"
+        // todo: add date to name
+        download="daily-notes.json"
+      >
+        <DocumentDownloadIcon />
+      </IconGrayLink>
+    );
+  }
+
+  if (isFetching) {
+    return (
+      <div className="relative cursor-wait text-gray-500" aria-busy="true">
+        <DocumentIcon className="h-8 w-8" role="presentation" />
+        <div className="absolute bottom-1.5 inset-x-2 animate-spin-slow">
+          <RefreshIcon
+            className="h-4 w-4 text-gray-400 scale-x-[-1]"
+            role="presentation"
+          />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <IconGrayButton
+      aria-label="Generate download for notes"
+      onClick={() => {
+        setState("load");
+      }}
+    >
+      <ClipboardCopyIcon className="translate-x-[0.1rem]" />
+    </IconGrayButton>
+  );
+};
+
+export default DownloadNotes;
