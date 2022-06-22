93b37a5f66d8124df0f8d13780dbabc0160cb3f6
andycarrell <carrellandrew@gmail.com>
2022-06-23 07:51:28 +1200

    Migrate more files to TS, daily notes, date reducer and search params

---
+++ src/utilities/useSearchParam.ts
@@ -0,0 +1,26 @@
+import { useMemo } from "react";
+
+const useSearchParam = (param: string) =>
+  useMemo(() => {
+    const get = () => {
+      const url = new URL(window.location.href);
+      const value = url.searchParams.get(param);
+      return value;
+    };
+
+    const set = (value: string) => {
+      const url = new URL(window.location.href);
+      url.searchParams.set(param, value);
+      window.history.pushState("", "", url.href);
+    };
+
+    const _delete = () => {
+      const url = new URL(window.location.href);
+      url.searchParams.delete(param);
+      window.history.pushState("", "", url.href);
+    };
+
+    return [get, set, _delete] as const;
+  }, [param]);
+
+export default useSearchParam;
